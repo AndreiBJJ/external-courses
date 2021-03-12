22d625220718335ab55405b0f6063ca6958ca3fa
apOprya <1998andreiopr1998@gmail.com>
2021-03-12 20:28:29 +0300

    ex2_js-basics-part2_change_task_03_&_04

--- src/ex2_js-basics-part2/task-03.js
+++ src/ex2_js-basics-part2/task-03.js
@@ -4,28 +4,28 @@ function arrayInConsole (userArray) {
   let count0 = 0;
     
   userArray.forEach(function (item) {
-  let typeOfElementArr = typeof item;
+    let typeOfElementArr = typeof item;
 
-  if ((typeOfElementArr !== "number") || ( isNaN(item))) {
-    return;
-  }
+    if ((typeOfElementArr !== "number") || ( isNaN(item))) {
+      return;
+    }
 
-  if (item === 0) {
-    count0 += 1;
-    return;
-  }
+    if (item === 0) {
+      count0 += 1;
+      return;
+    }
 
-  if (item % 2 === 0) {
-    countMod0 += 1;
-  }
+    if (item % 2 === 0) {
+      countMod0 += 1;
+    }
     
-  if (item % 2 !== 0) {
-    countModNot0 += 1;
-  }
+    if (item % 2 !== 0) {
+      countModNot0 += 1;
+    }
 
   });
 
-  let newUserArray = [countMod0,countModNot0,count0];
+  let newUserArray = [countMod0, countModNot0, count0];
   console.log(newUserArray);
   return newUserArray;
 }
--- src/ex2_js-basics-part2/task-04.js
+++ src/ex2_js-basics-part2/task-04.js
@@ -2,13 +2,12 @@ function checkValuesOfArray(arr) {
   
   for (let i = 0 ; i < arr.length - 1 ; i++) {
 
-  if (arr[i] === arr[i+1]) {
-    continue;
-  } else {
-    return false;
-  }
+    if (arr[i] !== arr[i+1]) {
+      return false;
+    }
 
   }
+  
   return true;
 }
 module.exports = checkValuesOfArray;
